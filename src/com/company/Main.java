bec0c92e817963865df58d9f9e79602aa4520fd6
anako <hope.love.faith87@gmail.com>
2021-11-03 20:14:55 -0400

    the blue ferrari car better then lambo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Car newCar=new Car();
+        newCar.name="Ferrariiiii";
+        newCar.color="blue";
+        newCar.doors=3;
+        newCar.engineCapacity="60000cc";
+        System.out.println(newCar.color);
 
     }
 }
